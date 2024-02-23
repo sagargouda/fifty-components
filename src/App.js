df06e000d6c8b13120aea28e9860d52fdb62a3c7
sagar <sagarc2580@gmail.com>
2024-02-23 21:41:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Accordian from "./components/accordian/accordian";
+function App() {
+  return (
+    <div className="App">
+      <Accordian/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/accordian/accordian.js
@@ -0,0 +1,9 @@
+import './accordian.css'
+
+function Accordian(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Accordian;
