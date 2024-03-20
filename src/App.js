b62659f69d7009da2b6d3adfd5f1b893430a7ef0
sagar <sagarc2580@gmail.com>
2024-03-20 12:35:00 +0530

    feat: loadmore component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Accordian from "./components/accordian/Accordian";
-import Starrating from "./components/starRating/starrating";
+// import Accordian from "./components/accordian/Accordian";
+// import Starrating from "./components/starRating/starrating";
+
+import LoadMore from "./components/loadmore/LoadMore";
 
 function App() {
   return (
       <div>
         {/*<Accordian/>*/}
-          <Starrating noOfStar={10}/>
+        {/*  <Starrating noOfStar={10}/>*/}
+          <LoadMore/>
       </div>
   );
 }
